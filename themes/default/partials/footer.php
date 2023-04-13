c559a138de8a810605c1de6fcae05b84225869cc
Alexandre Ducobu <alexandre.ducobu@pm.me>
2023-04-13 20:14:44 +0200

    Update footer

--- themes/default/partials/footer.php
+++ themes/default/partials/footer.php
@@ -1,9 +1,10 @@
-<footer class="footer mt-auto py-3 bg-body-tertiary">
+<footer class="footer mt-auto sticky-bottom  py-3 bg-body-tertiary">
     <div class="container">
     <span class="text-muted">
-      <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      <strong>Bootstrap</strong> by the <a href="https://getbootstrap.com/docs/5.3/about/team/">Bootstrap team</a> with
+        the help of our <a href="https://github.com/twbs/bootstrap/graphs/contributors">contributors</a>.
+        Code licensed <a href="https://github.com/twbs/bootstrap/blob/main/LICENSE">MIT</a>,
+        docs <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.
     </span>
     </div>
 </footer>
